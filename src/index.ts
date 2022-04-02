efbb6af54f5af38a3c937373a43a2a9ad185d497
Adam Prax <adam.prax@icloud.com>
2022-04-02 09:51:47 -0700

    Adds more shell integration

--- src/index.ts
+++ src/index.ts
@@ -3,64 +3,45 @@ import meow from "meow";
 import printNodes from "./printNodes/index.js";
 // const colors = require('colors');
 
-console.log(Date());
-/*
-printNodes([100, 2]);
-printNodes([1, 2, 3]);
-printNodes([1, 2, 13]);
-printNodes([1, 2, 3, 4]);
+const usage = `
+Usage
+  $ array-to-ascii-tree <json_array>
 
-printNodes([1, 2, 30000000000000, 205, 5, 14, 7, 5, 4, 9, 2]);
-printNodes([1, 2, 3, 4, 5, 6]);
-printNodes([1, 2, 3, 4, 5, 6, 7]);
-printNodes([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-printNodes([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 1]);
-printNodes([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 1, 2, 3, 4, 5, 6, 7]);
-printNodes([
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-]);
-printNodes([
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
-  23, 24, 25, 26, 27, 28, 29, 30, 31, 32,
-]);
-*/
-/*
-printNodes([
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
-  23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42,
-  43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62,
-  63, 64, 65, 66, 67, 68, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74,
-  75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94,
-  95, 96, 97, 98, 99
-]);
-*/
+Options
+  --json, -j  Output tree to JSON string
 
-const { input, flags } = meow(
-  `
-	Usage
-	  $ array-to-ascii-tree <json_array>
+Examples
+  $ array-to-ascii-tree [1,2,3]
+      1
+    /   \\
+   /     \\
+  2       3
 
-	Options
-	  --json, -j  Output tree to JSON string
-
-	Examples
-	  $ array-to-ascii-tree [1,2,3]
-	       1
-       /   \
-      /     \
-     2       3
-`,
-  {
-    importMeta: import.meta,
-    flags: {
-      file: {
-        type: "boolean",
-        alias: "j",
-      },
+`;
+const { input, flags } = meow(usage, {
+  importMeta: import.meta,
+  flags: {
+    json: {
+      type: "boolean",
+      alias: "j",
     },
-  }
-);
+  },
+});
 
-console.log(JSON.parse(input[0] || "[]"));
-printNodes([], flags);
-console.info("End of program");
+const parsedInput: string[] = JSON.parse(input[0] || "[]");
+if (parsedInput?.length <= 1 ?? true) {
+  console.error("---Array must be greater than a length of 1---");
+  process.exit(1);
+}
+const output = printNodes(parsedInput);
+if (!output) {
+  process.exit(1);
+}
+if (flags.json) {
+  process.stdout.write(JSON.stringify(output));
+} else {
+  for (const row of output) {
+    console.log(row.join(""));
+  }
+}
+process.exit(0);
--- src/printNodes/index.ts
+++ src/printNodes/index.ts
@@ -1,24 +1,9 @@
-import type { TypedFlags } from "meow";
-
-const printNodes = (
-  numbers: number[],
-  flags?: TypedFlags<{
-    file: {
-      type: "boolean";
-      alias: string;
-    };
-  }> &
-    Record<string, unknown>
-) => {
-  console.log(flags);
-  if (numbers.length < 1) {
-    return console.error("---No arguments passed---");
-  }
-
-  if (numbers.length === 1) {
-    return console.log(numbers[0]);
-  }
-  let cellLength = Math.max(...numbers).toString().length;
+const printNodes: (
+  numbers: number[] | string[]
+) => string[][] | number[][] | null = (numbers) => {
+  let cellLength = Math.max(
+    ...numbers.map((number) => number.toString().length)
+  );
   if (cellLength % 2 === 0) {
     cellLength++;
   }
@@ -32,26 +17,28 @@ const printNodes = (
   }
   const branchWidth = 2 * branchHeight - 1;
 
-  const output = Array(branchHeight);
+  const output = Array<string[]>(branchHeight);
 
   for (let x = 0; x < output.length; x++) {
-    output[x] = Array(branchWidth).fill(" ".padEnd(Math.ceil(cellLength)));
+    output[x] = Array<string>(branchWidth).fill(
+      " ".padEnd(Math.ceil(cellLength))
+    );
   }
 
   const colPos = Math.floor(branchWidth / 2);
   const branchCount = Math.pow(2, height);
 
   const root = numbers[0] || 0;
-  output[0][colPos] = root
+  output[0]![colPos] = root
     .toString()
     .padStart(Math.ceil(cellLength / 2))
     .padEnd(cellLength);
 
   for (let y = 1; y <= branchCount; y++) {
-    output[y][colPos - y] = "/"
+    output[y]![colPos - y] = "/"
       .padStart(Math.ceil(cellLength / 2))
       .padEnd(cellLength);
-    output[y][colPos + y] = "\\"
+    output[y]![colPos + y] = "\\"
       .padEnd(Math.ceil(cellLength / 2))
       .padStart(cellLength);
   }
@@ -77,7 +64,7 @@ const printNodes = (
       numbers[x] = 0;
     }
     const value = numbers[x] || 0;
-    output[rowPosition][colPosition] = isLeft
+    output[rowPosition]![colPosition] = isLeft
       ? value
           .toString()
           .padStart(Math.ceil(cellLength / 2))
@@ -91,19 +78,16 @@ const printNodes = (
     if (2 * x + 1 < numbers.length) {
       const branchCount = Math.pow(2, height - rowLevel);
       for (let y = rowPosition + 1; y <= rowPosition + branchCount; y++) {
-        output[y][colPosition - y + rowPosition] = "/"
+        output[y]![colPosition - y + rowPosition] = "/"
           .padStart(Math.ceil(cellLength / 2))
           .padEnd(cellLength);
-        output[y][colPosition + y - rowPosition] = "\\"
+        output[y]![colPosition + y - rowPosition] = "\\"
           .padEnd(Math.ceil(cellLength / 2))
           .padStart(cellLength);
       }
     }
   }
-  for (const row of output) {
-    console.log(row.join(""));
-  }
-  console.log("");
+
   return output;
 };
 
