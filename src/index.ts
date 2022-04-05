220cd44fd1f38a89a644b8044fc8bc1eb004ea3f
aprax <adam.prax@gmail.com>
2022-04-04 19:27:28 -0700

    Restores CLI code. (#6)

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,8 @@
 #!/usr/bin/env node
-// import meow from "meow";
+import meow from "meow";
 import printNodes from "./printNodes/index.js";
 import toConsole from "./toConsole/index.js";
-// const colors = require('colors');
 
-const MAX_LEVEL = 7; // Currently does not work for MAX_LEVEL >= 4
-/*
-for (let x = MAX_LEVEL; x >= 0; x--) {
-  const totalNodes = Math.pow(2, MAX_LEVEL - x) ;
-  const numbers = Array(totalNodes);
-  for (let y = 1; y <= totalNodes; y++) {
-    numbers[y - 1] = y;
-  }
-
-  const actual = printNodes(numbers);
-
-  toConsole(actual);
-  console.log('')
-}
-*/
-for (let length = 1; length <= Math.pow(2, MAX_LEVEL); length++) {
-  const numbers = Array(length);
-  for (let y = 0; y <= numbers.length; y++) {
-    numbers[y - 1] = y;
-  }
-  const actual = printNodes(numbers);
-  toConsole(actual);
-  console.log("");
-}
-
-/*
 const usage = `
 Usage
   $ a2b <json_array>
@@ -71,4 +44,3 @@ if (flags.json) {
   toConsole(output);
 }
 process.exit(0);
-*/
