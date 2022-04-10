237c956d24fe27a40544e6a3ace59d61e3ad6415
Adam Prax <adam.prax@gmail.com>
2022-04-09 22:39:11 -0700

    Separates CLI and library exports.

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,6 @@
-#!/usr/bin/env node
-import meow from "meow";
 import printNodes from "./printNodes/index.js";
 import toConsole from "./toConsole/index.js";
 
 export default printNodes;
 
 export { toConsole };
-
-const usage = `
-Usage
-  $ a2b <json_array>
-
-Options
-  --json, -j  Output tree to JSON string
-
-Examples
-  $ a2b [1,2,3]
-      1
-    /   \\
-   /     \\
-  2       3
-
-`;
-
-const { input, flags } = meow(usage, {
-  importMeta: import.meta,
-  flags: {
-    json: {
-      type: "boolean",
-      alias: "j",
-    },
-  },
-});
-
-const parsedInput: string[] = JSON.parse(input[0] || "[]");
-if (parsedInput?.length <= 1 ?? true) {
-  throw Error("---Array must be greater than a length of 1---");
-}
-const output = printNodes(parsedInput);
-if (!output) {
-  process.exit(1);
-}
-if (flags.json) {
-  process.stdout.write(JSON.stringify(output));
-} else {
-  toConsole(output);
-}
-process.exit(0);
